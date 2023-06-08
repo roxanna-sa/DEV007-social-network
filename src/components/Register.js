38a0d09ff8afbbff66884383b35e75f3604a189f
iceror <ilsecervor@gmail.com>
2023-06-08 16:02:55 -0600

    create routes && initial functions

---
+++ src/components/Register.js
@@ -0,0 +1,10 @@
+//registro datos usuario
+export const Register = () => {
+  const RegisterDiv = document.createElement('div');
+  RegisterDiv.textContent = 'Ingresa tus datos';
+  const joinButton = document.createElement('button');
+  joinButton.textContent = 'Unirse';
+
+  //TO DO Inputs nombre, fecha nac, patologia 
+  return RegisterDiv;
+}

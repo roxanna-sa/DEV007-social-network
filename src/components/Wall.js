b79e471285fa9362bfd5147b50e1107b71d118fb
iceror <ilsecervor@gmail.com>
2023-06-19 12:25:02 -0600

    change html elements creation

--- src/components/Wall.js
+++ src/components/Wall.js
@@ -1,4 +1,3 @@
-import { async } from "regenerator-runtime";
 import { logOut } from "../lib/auth";
 import { createPost, getPosts } from "../lib/firestore";
 //muro personal
@@ -8,87 +7,74 @@ export const Wall = (onNavigate) => {
 
   const getUser = localStorage.getItem('user');
   //console.log("Obteniendo el usuario en local storage..", localStorage.getItem('user'));
-
   /* Cuando en localStorate["Usuario"] revisamos en firebase que esa id exista y traemos los datos del usuario, mostrandole el timeline  */
   if (getUser) {
     // Verificar con firebase...
-
     // Si no es válido eliminar todo localStorage con localStorage.clear() y enviar de nuevo a la misma página.npm s
-    const welcome = document.createElement("p");
-    welcome.textContent = '¡Bienvenido ' + localStorage.getItem('name') + '!';
     const divUserAndSearch = document.createElement('div');
     divUserAndSearch.className = 'divUserAndSearch';
-    const userAccount = document.createElement('img');
-    userAccount.src = '../img/user.png';
-    userAccount.className = ' userAccount';
-    const searchInput = document.createElement('input');
-    searchInput.placeholder = 'Buscar';
-    searchInput.setAttribute('type', 'search');
-    searchInput.className = 'searchInput';
+    divUserAndSearch.innerHTML = `
+    <img src='../img/user.png' class='userAccount'>
+    <input placeholder="Buscar" type="search" class="searchInput">
+    `
 
     // TODO añadir lupa a search input
-    const lupa = document.createElement('div');
-    lupa.className = 'lupa';
-    // lupa.src = '../img/lupa.png';
-    searchInput.appendChild(lupa);
+    // const lupa = document.createElement('div');
+    // lupa.className = 'lupa';
+    // // lupa.src = '../img/lupa.png';
 
-    divUserAndSearch.appendChild(userAccount);
-    divUserAndSearch.appendChild(searchInput);
-    divUserAndSearch.appendChild(welcome);
-
-    //Espacio para post
+    const publishPostInputAndButton = document.createElement('div');
+    publishPostInputAndButton.className = 'publishPostInputAndButton'
+    publishPostInputAndButton.innerHTML = `
+    <input class='postInput' id='postInput'></input>
+    <button class='publishButton' id='publishButton'>Publicar</button>
+    `;
+    //Espacio para posts
     const divPost = document.createElement('div');
     divPost.className = 'divPost';
 
-    const postInput = document.createElement('input');
-    postInput.id = 'postInput';
-    postInput.className = 'postInput';
-    const publishPost = document.createElement('button');
-    publishPost.className = 'publishPost';
-    publishPost.textContent = 'Publicar';
-    divPost.appendChild(postInput);
-    divPost.appendChild(publishPost);
+    document.addEventListener('DOMContentLoaded', () => {
+      let publishButton = document.getElementById('publishButton');
 
-    publishPost.addEventListener('click', async () => {
-      const inputText = postInput.value;
-      await createPost(inputText);
+      publishButton.addEventListener('click', async () => {
+        const inputText = postInput.value;
 
-      const post = document.createElement('div');
-      post.className = 'post';
-      post.textContent = inputText;
+        if (inputText.trim() === '') {
+          alert('Debes escribir algo para publicar...');
+        } else {
+        await createPost(inputText);
 
-      const userName = document.createElement('div');
-      userName.className = 'userName';
-      userName.textContent = localStorage.getItem('name');
+        const post = document.createElement('div');
+        post.className = 'post';
+        post.innerHTML = `
+        <div class='userName' id='userName'>${localStorage.getItem('name')}</div>
+        <button><img src='../img/menu.png' class='menuPost'></button>
+        <p> ${inputText}</p>
+        `
+        divPost.appendChild(post);
+        clearInput();
+        }
+      });
 
-      post.appendChild(userName);
-      divPost.appendChild(post);
-
-      clearInput();
+      function clearInput() {
+        //console.log('borrando el input');
+        document.getElementById("postInput").value = '';
+      }
     });
-
-    function clearInput() {
-      //console.log('borrando el input');
-      document.getElementById("postInput").value = '';
-    }
 
     //Muestra todos los posts ya guardados en firestore
     async function showAllPosts() {
       let arrayPosts = await getAllPosts();
-      //console.log(arrayPosts);
       arrayPosts.forEach(post => {
-        //console.log(post);
         const singlePost = document.createElement('div');
-        const userName = document.createElement('div');
-        userName.className = 'userName';
-        userName.textContent = post.userName;
 
-        singlePost.textContent = post.postContent;
-        singlePost.appendChild(userName);
+        singlePost.innerHTML = `
+        <div class="userName">${post.userName}<button><img src='../img/menu.png'class='menuPost'></button></div>
+        
+        <p>${post.postContent}</p>
+        `;
 
-        //console.log(singlePost);
-        divPost.appendChild(singlePost)
-
+        divPost.appendChild(singlePost);
       });
     }
 
@@ -97,22 +83,12 @@ export const Wall = (onNavigate) => {
     //Menu 
     const divMenu = document.createElement('div');
     divMenu.className = 'divMenu';
-    const homeButton = document.createElement('button');
-    const homeIcon = document.createElement('img');
-    homeIcon.src = '../img/home.png';
-    homeButton.appendChild(homeIcon);
-    divMenu.appendChild(homeButton);
-    const postButton = document.createElement('button');
-    const postIcon = document.createElement('img');
-    postIcon.src = '../img/add-post.png';
-    postIcon.className = 'postIcon';
-    postButton.appendChild(postIcon);
-    divMenu.appendChild(postButton);
-    const friendsButton = document.createElement('button');
-    const friendsIcon = document.createElement('img');
-    friendsIcon.src = '../img/friends.png';
-    friendsButton.appendChild(friendsIcon);
-    divMenu.appendChild(friendsButton);
+
+    divMenu.innerHTML = `
+    <button><img src='../img/home.png'></button>
+    <button><img src='../img/add-post.png' class='postIcon'></button>
+    <button><img src='../img/friends.png'></button>
+    `
 
     const logOutButton = document.createElement('button');
     logOutButton.textContent = 'Cerrar sesión';
@@ -123,34 +99,34 @@ export const Wall = (onNavigate) => {
     });
 
     WallDiv.appendChild(divUserAndSearch);
+    WallDiv.appendChild(publishPostInputAndButton);
     WallDiv.appendChild(divPost);
     WallDiv.appendChild(divMenu);
     WallDiv.appendChild(logOutButton);
 
   } else {
     /* El usuario NO está logueado, por lo tanto sólo ve una página estática en la que le decimos que se registre o inicie sesión */
-    const notLoggedText = document.createElement('h2');
-    notLoggedText.textContent = 'Hola soy home, no estás loguead@';
-    const logInButton = document.createElement('button');
-    logInButton.textContent = 'Inicio';
-    const registerButton = document.createElement('button');
-    registerButton.textContent = 'Registrar';
+    const notLoggedUser = createHtmlElement(`
+  <div>
+    <h2> Bienvenido a Nutrivid, inicia sesión o regístrate </h2>
+    <div>
+      <button>Inicio</button>
+      <button>Registrar</button>
+    </div>
+  </div>
+`);
 
-    const buttonsDiv = document.createElement('div');
-    buttonsDiv.className = 'buttons-div';
-    buttonsDiv.appendChild(logInButton);
-    buttonsDiv.appendChild(registerButton);
-    WallDiv.appendChild(notLoggedText);
-    WallDiv.appendChild(buttonsDiv);
-
-    logInButton.addEventListener('click', () => { onNavigate('/') });
+    document.addEventListener('DOMContentLoaded', () => { 
+      logInButton.addEventListener('click', () => { onNavigate('/') });
     registerButton.addEventListener('click', () => { onNavigate('/register') });
-  }
+    });
+    WallDiv.appendChild(notLoggedUser);
+  };
   // TO DO inputs de email y password
   return WallDiv;
-}
+};
 
 async function getAllPosts() {
   //console.log(await getPosts());
   return await getPosts();
-}
+};
